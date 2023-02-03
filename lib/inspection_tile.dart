e5cf25d92fe1a958e6b994bf7a973c26f200054e
Hugo Fabris <hugomfabris@gmail.com>
2023-02-03 12:56:17 -0300

    Update inspection_tile.dart
    Updated the ListTile widget so that can be used in main.dart, providing better code organization. Also created the final historyInspection and added to the class constructor, to make it a History instance so that it can be used as a box in main.dart.

--- lib/inspection_tile.dart
+++ lib/inspection_tile.dart
@@ -1,22 +1,48 @@
-import 'package:bcsantos/inspections.dart';
+import 'package:bcsantos/model/history.dart';
 import 'package:flutter/material.dart';
+import 'package:bcsantos/shell_execute_service.dart';
+
 
 class InspectionTile extends StatelessWidget {
-  const InspectionTile({super.key, required this.inspection});
+  
+  const InspectionTile({super.key, required this.historyInspection});
 
-  final Inspection inspection;
+  final History historyInspection;
 
   @override
   Widget build(BuildContext context) {
+
     return Card(
-      child: ListTile(
-          leading: CircleAvatar(child: Text(inspection.inspector![0])),
-          title: Text(inspection.inspector!,
-              style: const TextStyle(fontSize: 24.0)),
-          subtitle: Text(
-              "${inspection.inspectionDate!} - ${inspection.inspectionType!} - ${inspection.anotacoes} anotações",
-              style: const TextStyle(fontSize: 16.0)),
-          trailing: const Icon(Icons.cloud_upload)),
-    );
+        child: ListTile(
+      title: Text(historyInspection.inspector!),
+      subtitle: Text(
+          '${historyInspection.inspectionDate.day.toString()}-${historyInspection.inspectionDate.month.toString()}-${historyInspection.inspectionDate.year.toString()}'),
+      leading: CircleAvatar(
+        child: Text(historyInspection.inspector![0]),
+      ),
+      trailing: IconButton(
+        icon: const Icon((Icons.cloud_upload)),
+        onPressed: () {
+          ShellExecuteService shellExecuteService = ShellExecuteService();
+          void openFile(path) async {
+            final filePath = path;
+            final result = await shellExecuteService.openFile(filePath);
+            if (result) {
+              (mounted) {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(const SnackBar(content: Text('File opened')));
+              };
+            } else {
+              (mounted) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('File not opened')));
+              };
+            }
+          }
+
+          return openFile(historyInspection.archive);
+        },
+      ),
+    ));
   }
 }
