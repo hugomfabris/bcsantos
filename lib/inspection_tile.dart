d3b6b8fdd0b5102ee709ea3a51b819a435a2e28b
Hugo Fabris <hugomfabris@gmail.com>
2023-02-05 13:31:57 -0300

    Update inspection_tile.dart
    Added DataTable to the ListTile

--- lib/inspection_tile.dart
+++ lib/inspection_tile.dart
@@ -1,78 +1,66 @@
-import 'package:bcsantos/model/history.dart';
+import 'package:bcsantos/models/history.dart';
 import 'package:flutter/material.dart';
 import 'package:bcsantos/shell_execute_service.dart';
 import 'package:hive/hive.dart';
 
-
 class InspectionTile extends StatelessWidget {
-  
   const InspectionTile({super.key, required this.historyInspection});
 
   final History historyInspection;
 
   @override
   Widget build(BuildContext context) {
-
     return Card(
         child: ListTile(
-      title: Text(historyInspection.inspector!),
-      subtitle: Text(
-          '${historyInspection.inspectionDate.day.toString()}-${historyInspection.inspectionDate.month.toString()}-${historyInspection.inspectionDate.year.toString()}'),
-      leading: CircleAvatar(
-        child: Text(historyInspection.inspector![0]),
-      ),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          IconButton(
-            icon: const Icon((Icons.fact_check)),
-            onPressed: () {
-              ShellExecuteService shellExecuteService = ShellExecuteService();
-              void openFile(path) async {
-              final filePath = path;
-              final result = await shellExecuteService.openFile(filePath);
-              if (result) {
-                (mounted) {
-                  ScaffoldMessenger.of(context)
-                    .showSnackBar(const SnackBar(content: Text('File opened')));
-              };
-            } 
-              else {
-                (mounted) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('File not opened')));
-                };
-              }
-          }
+            title: Center(
+                child: DataTable(columns: [
+              DataColumn(label: Text('BC/RB', textAlign: TextAlign.center)),
+              DataColumn(label: Text('Inspetor', textAlign: TextAlign.center)),
+              DataColumn(label: Text('Anotações', textAlign: TextAlign.center)),
+              DataColumn(label: Text('Tipo de Inspeção', textAlign: TextAlign.center)),
+              DataColumn(label: Text('Data', textAlign: TextAlign.center))
+            ], rows: [
+              DataRow(cells: [
+                DataCell(Text(historyInspection.name!, textAlign: TextAlign.center)),
+                DataCell(Text(historyInspection.inspector!, textAlign: TextAlign.center)),
+                DataCell(Text(historyInspection.anotations.toString(), textAlign: TextAlign.center)),
+                DataCell(Text(historyInspection.inspectionType!, textAlign: TextAlign.center)),
+                DataCell(Text(
+                    '${historyInspection.inspectionDate.day.toString()}-${historyInspection.inspectionDate.month.toString()}-${historyInspection.inspectionDate.year.toString()}', textAlign: TextAlign.center))
+              ])
+            ])),
+            leading: CircleAvatar(
+              child: Text(historyInspection.inspector![0]),
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                IconButton(
+                    icon: const Icon((Icons.file_upload)),
+                    onPressed: () {
+                      ShellExecuteService shellExecuteService =
+                          ShellExecuteService();
+                      void openFile(path) async {
+                        final filePath = path;
+                        final result =
+                            await shellExecuteService.openFile(filePath);
+                        if (result) {
+                          (mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('File opened')));
+                          };
+                        } else {
+                          (mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('File not opened')));
+                          };
+                        }
+                      }
 
-          return openFile(historyInspection.plan);
-        },
-      ),
-          IconButton(
-            icon: const Icon((Icons.file_upload)),
-            onPressed: () {
-              ShellExecuteService shellExecuteService = ShellExecuteService();
-              void openFile(path) async {
-              final filePath = path;
-              final result = await shellExecuteService.openFile(filePath);
-              if (result) {
-                (mounted) {
-                  ScaffoldMessenger.of(context)
-                    .showSnackBar(const SnackBar(content: Text('File opened')));
-              };
-            } 
-              else {
-                (mounted) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('File not opened')));
-                };
-              }
-          }
-          
-          return openFile(historyInspection.reportArchive);
-        })
-      ],
-      )
-    ));
+                      return openFile(historyInspection.checklist);
+                    })
+              ],
+            )));
   }
 }
