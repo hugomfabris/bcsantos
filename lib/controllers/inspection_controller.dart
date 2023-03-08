4f452f9899fe770fc5bdea0ae72db853c8408c78
Hugo Fabris <hugomfabris@gmail.com>
2023-03-08 19:34:44 -0300

    Update inspection_controller.dart
    Added: chipNames (brought from chips_controller, which will not be used anymore), a setter and a getter to inspections and the setFilter and clearFilters methods (to apply the logic when a chip is clicked by the user)

--- lib/controllers/inspection_controller.dart
+++ lib/controllers/inspection_controller.dart
@@ -1,26 +1,37 @@
-import 'package:bcsantos/inspection_tile.dart';
 import 'package:bcsantos/models/hive_models.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class InspectionController extends ChangeNotifier {
-
   InspectionController() {
     init();
   }
 
   final box = Hive.box<Inspection>('inspectionBox');
-  final List<Inspection> _inspections = [];
+  late List<Inspection> _inspections = [];
+  final List<String> _chipsNames = [];
 
   void init() {
     for (var element in box.values) {
       _inspections.add(element);
+      if (_chipsNames.contains(element.name) == false) {
+        _chipsNames.add(element.name.toString());
+      }
     }
   }
 
   List<Inspection> get inspections {
     return _inspections;
   }
+
+  set inspections(List<Inspection> inspections) {
+    _inspections = inspections;
+    notifyListeners();
+  }
+
+  List<String> get chipsNames {
+    return _chipsNames;
+    }
 
   void addInspection(Inspection inspection) {
     _inspections.add(inspection);
@@ -32,7 +43,15 @@ class InspectionController extends ChangeNotifier {
     _inspections.remove(inspection);
     notifyListeners();
     inspection.delete();
-    
-    
+  }
+
+  setFilter(String name) {
+    _inspections = _inspections.where((element) => element.name == name).toList();
+    notifyListeners();
+  }
+
+  clearFilters() {
+    init();
+    notifyListeners();
   }
 }
